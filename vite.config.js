0d42592142cb0929211fe711858c5809974296fe
Doublechest <twin01v@gmail.com>
2024-12-15 17:44:30 +0300

    fix shit

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   base: `/SS14.Status/`,
+  publicDir: 'SS14.Status',
   plugins: [
     vue(),
     vueDevTools(),
